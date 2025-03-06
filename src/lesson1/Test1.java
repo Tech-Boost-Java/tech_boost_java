8b12aa1eb25562c7b5b7e581bff38fa2e212e317
Faryna Arsenii <farynaarseniy2007@gmail.com>
2025-03-06 20:08:50 +0200

    First Branch

--- src/lesson1/Test1.java
+++ src/lesson1/Test1.java
@@ -2,6 +2,7 @@ package lesson1;
 
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("Hello, Test1");
+
+        System.out.println("Hello, it's Arsenii");
     }
 }
