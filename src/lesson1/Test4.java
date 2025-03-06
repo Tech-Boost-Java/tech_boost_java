fee53770b6f102fc39e13884c0a8decc0a94792a
bubelnyk_yura <urabubelnik@gmail.com>
2025-03-06 19:58:53 +0200

    first_commit

--- src/lesson1/Test4.java
+++ src/lesson1/Test4.java
@@ -2,6 +2,6 @@ package lesson1;
 
 public class Test4 {
     public static void main(String[] args) {
-        System.out.println("Hello, Test4");
+        System.out.println("Hello, Test4 + 1");
     }
 }
