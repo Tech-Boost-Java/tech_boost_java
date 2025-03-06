9c39211b1a941e01cbe7115a505c32423e9c993b
Nazarii_Vykovych <nazarko1706@gmail.com>
2025-03-06 19:58:36 +0200

    new branch+text change

--- src/lesson1/Test4.java
+++ src/lesson1/Test4.java
@@ -3,5 +3,6 @@ package lesson1;
 public class Test4 {
     public static void main(String[] args) {
         System.out.println("Hello, friends");
+        System.out.println("Hello, friends");
     }
 }
