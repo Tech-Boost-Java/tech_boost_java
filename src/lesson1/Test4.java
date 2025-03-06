464a59c25d1587ffbe8222b8eda4c83fd7fc049e
OlhenShu <olya.shutileva@gmail.com>
2025-03-06 19:58:56 +0200

    was added new branch Olhaa

--- src/lesson1/Test4.java
+++ src/lesson1/Test4.java
@@ -3,5 +3,6 @@ package lesson1;
 public class Test4 {
     public static void main(String[] args) {
         System.out.println("Hello, Test4");
+        System.out.println("Hello, Test4");
     }
 }
