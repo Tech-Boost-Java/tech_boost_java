46955ddafd2c4077ef2bae3eda25f975aa4c83cd
ruslanapash <ruslanarusya2007@gmail.com>
2025-03-08 11:07:20 +0200

    First commit

--- src1/Test1_1.java
+++ src1/Test1_1.java
@@ -1,6 +1,6 @@
 public class Test1_1 {
     public static void main(String[] args) {
         System.out.println("Hello, Test1");
-        System.out.println("Привіт");;
+        System.out.println("Привіт");
     }
 }
