f343933c666138dbb2877066ebd46862bdce259f
Michaelnotme <mishabos2103@gmail.com>
2025-03-09 10:47:50 +0200

    Test commit (Mykhaylo Borshchevskyi)

---
+++ src/Tester/TestClass.java
@@ -0,0 +1,9 @@
+package Tester;
+
+public class TestClass {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, TestClass!");
+        System.out.println("Hello, This is Michael)))");
+    }
+}
