325b787497f54647b70fbea66f4d5ee07f3370d6
Markiian Lytvyn <markiian.lytvyn.shi.2024@lpnu.ua>
2025-03-11 17:29:35 +0200

    Testing1

--- Testing_test.java
+++ Testing_test.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
 
-public class Testing_test {
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
 }
