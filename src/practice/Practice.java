199077a0ee8307baf474ea8746dd8023aa67160d
Lopatin Volodymyr <lopatinvova13@gmail.com>
2025-03-13 19:30:11 +0200

    Lopatin Volodymyr - commit 1

---
+++ src/practice/Practice.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class Practice {
+    public static void main(String[] args) {
+        System.out.println("Hello, Practice");
+        byte x = 1;
+        String y = "We live only ";
+        System.out.println(y + x + " time.");
+    }
+}
