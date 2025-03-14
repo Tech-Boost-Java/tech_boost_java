400cd58d7d9064c07f0990e86c09638a5e1ff1ed
OlhenShu <olya.shutileva@gmail.com>
2025-03-14 17:01:17 +0200

    changed main branch

--- src/lesson1/Test1.java
+++ src/lesson1/Test1.java
@@ -2,6 +2,7 @@ package lesson1;
 
 public class Test1 {
     public static void main(String[] args) {
+        System.out.println("It`s an example of the first class");
         System.out.println("Hello, Test1");
     }
 }
