8a9bb9473f99436bc3305dbcc00e6a12a91f0c5a
Zlata Dovbyk <dovbykzlata@gmail.com>
2025-03-21 00:07:53 +0200

    added a new class

---
+++ src/hw/DigitsSum.java
@@ -0,0 +1,19 @@
+package hw;
+
+public class DigitsSum {
+    public static void main(String[] args) {
+        System.out.println(calculateDigitSum(123)); // Очікуваний результат: 6
+    }
+    public static int calculateDigitSum(int number) {
+        int sum = 0;
+        if (number < 100 || number > 999) {
+            throw new IllegalArgumentException("The input number is not a three-digit number.");
+        }
+        while (number > 0) {
+            int part = number % 10;
+            sum += part;
+            number /= 10;
+        }
+        return sum;
+    }
+}
