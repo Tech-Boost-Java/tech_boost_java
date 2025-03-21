87766a1efaefb9ae96f2861cad77e51cd3482b6c
Eduard Sierov <sierov_eduard@knu.ua>
2025-03-21 21:32:20 +0200

    Finished 2 topic

---
+++ src/Practical1/MathOperations.java
@@ -0,0 +1,28 @@
+package Practical1;
+
+import java.util.Scanner;
+
+import static java.lang.Math.*;
+
+public class MathOperations {
+    public static double calculateSquareRoot(double num) {
+        return (double) sqrt(num);
+    }
+
+    public static double calculateSquare(double num) {
+        return (double) pow(num, 2);
+    }
+
+    public static double calculateCube(double num) {
+        return (double) pow(num, 3);
+    }
+
+
+
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    double number = sc.nextDouble();
+    double sum = calculateCube(number);
+    System.out.println(sum);
+}
+}
