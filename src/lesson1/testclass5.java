543434f961954f9d37eea0134a73021f4bc87fa8
Anna Komar <anakomar586@gmail.com>
2025-03-21 21:04:12 +0200

    Rectangle

--- src/lesson1/testclass5.java
+++ src/lesson1/testclass5.java
@@ -1,4 +1,13 @@
 package lesson1;
 
 public class testclass5 {
+    public static int calculateArea(int width, int height) {
+        return width * height;
+    }
+
+    public static void main(String[] args) {
+        int width = 5;
+        int height = 10;
+        System.out.println("Площа прямокутника: " + calculateArea(width, height));
+    }
 }
