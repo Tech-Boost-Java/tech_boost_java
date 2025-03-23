58355cb7123972c623a1fcad8c6aeb07e534ea36
Dirik_Ali <zon.v82221@gmail.com>
2025-03-23 22:17:29 +0200

    TestClass.java has been added

---
+++ src/lesson1/TestClass.java
@@ -0,0 +1,8 @@
+package lesson1;
+
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("It`s an example of the second class");
+        System.out.println("Hello, TestClass");
+    }
+}
