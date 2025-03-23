810cb1f367b116573735ada66516b98c09f85370
LoFi_SwanL <andrii.itsko@knu.ua>
2025-03-23 18:30:52 +0200

    First practical task is done

---
+++ src/PracricalTask1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class PracricalTask1 {
+    static Scanner sc = new Scanner(System.in);
+
+    static int getNumber(String prompt){
+        System.out.print(prompt);
+        return sc.nextInt();
+    }
+    static int getTotal(int a, int b){
+        return a + b;
+    }
+    static float getAverage(float a, float b){
+        return (a + b) / 2;
+    }
+
+    public static void main(String[] args) {
+        int firstNumb = getNumber("Enter the first number: ");
+        int secondNumb = getNumber("Enter the second number: ");
+
+        int sum = getTotal(firstNumb, secondNumb);
+        System.out.println("The sum of " + firstNumb + " and " + secondNumb + " is " + sum);
+
+        double average = getAverage(firstNumb, secondNumb);
+        System.out.println("The average of " + firstNumb + " and " + secondNumb + " is " + average);
+
+
+    }
+}
