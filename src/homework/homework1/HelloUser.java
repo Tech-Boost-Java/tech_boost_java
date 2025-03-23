4e6690a1f74446ac267a8476cb9f055583802d98
Danoslo 255 <danya.nikitin7@gmail.com>
2025-03-23 15:44:32 +0200

    test

---
+++ src/homework/homework1/HelloUser.java
@@ -0,0 +1,16 @@
+package homework.homework1;
+
+import java.util.Scanner;
+
+public class HelloUser {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите ваше имя: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Привет, " + name + "!");
+
+        scanner.close();
+    }
+}
