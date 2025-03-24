49944d273f40fb467c2b38bc7813ff0c0013cf19
Zlata Dovbyk <dovbykzlata@gmail.com>
2025-03-24 19:28:31 +0200

    added a new class

--- src/hw/BankAccount.java
+++ src/hw/BankAccount.java
@@ -19,6 +19,25 @@ public class BankAccount {
     public double getBalance() {
         return balance;
     }
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+        else {
+            System.out.println("Deposit amount must be positive.");
+        }
+    }
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        }
+        else if (amount <= 0) {
+            System.out.println("Withdrawal amount must be positive.");
+        }
+        else {
+            System.out.println("Insufficient balance.");
+        }
+    }
     public static void main(String[] args) {
         BankAccount account = new BankAccount("123456789", 54000.50);
         System.out.println("Account Number: " + account.getAccountNumber());
@@ -27,5 +46,12 @@ public class BankAccount {
         System.out.println("Updated Account Number: " + account.getAccountNumber());
         account.setBalance(75700.75);
         System.out.println("Updated Balance: " + account.getBalance());
+        account.deposit(5000.00);
+        System.out.println("Balance after deposit: " + account.getBalance());
+        account.withdraw(2000.00);
+        System.out.println("Balance after withdrawal: " + account.getBalance());
+        account.withdraw(80000);
+        account.deposit(-100);
+        account.withdraw(-200);
     }
 }
