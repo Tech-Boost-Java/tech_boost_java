d57f829b4f9f271572eface020c8fec6d7e29f3a
LoFiSwanL <daiki.ren710@gmai.com>
2025-04-08 21:21:06 +0300

    small chokolates

--- src/PracricalTask1.java
+++ src/PracricalTask1.java
@@ -16,7 +16,7 @@ public class PracricalTask1 {
 
     public static void main(String[] args) {
         int firstNumb = getNumber("Enter the first number: ");
-        int secondNumb = getNumber("Enter the second number: ");
+        int secondNumb = getNumber("Enter the second number1: ");
 
         int sum = getTotal(firstNumb, secondNumb);
         System.out.println("The sum of " + firstNumb + " and " + secondNumb + " is " + sum);
