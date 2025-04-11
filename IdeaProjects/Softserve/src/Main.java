8b0b0f7850f815437d9d33d98202e4a4e6f49728
Malakhova Kateryna <malahovakata638@gmail.com>
2025-04-11 22:28:55 +0300

    Delete IdeaProjects/Softserve directory

--- IdeaProjects/Softserve/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        AreaOfATriangle areaOfATriangle = new AreaOfATriangle();
-        areaOfATriangle.inputSide();
-        areaOfATriangle.printResult();
-    }
-}
