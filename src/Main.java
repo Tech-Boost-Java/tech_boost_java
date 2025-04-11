087985bf45b5e7588f6c74ab15b2619206d4d5a1
extziz <malahovakata638@gmail.com>
2025-04-11 21:15:54 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.input();
+        person.output();
+        person.changeName("John", "Dou");
+        person.output();
+    }
+}
