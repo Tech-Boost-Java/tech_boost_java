9f96bfe65fe6e039bb049642f8e90232f38802ad
LoFiSwanL <daiki.ren710@gmai.com>
2025-04-14 13:29:00 +0300

    a piece of Homework №7

---
+++ src/HW74.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class HW74 {
+    static Map<String, String> personMap(String[] surname, String[] name){
+        Map<String, String> map = new HashMap<>();
+        for(int i = 0; i < surname.length; i++){
+            map.put(surname[i], name[i]);
+        }
+        return map;
+    }
+    public static void main(String[] args) {
+        String[] surnames = {"Smith", "Johnson", "Brown", "Taylor", "Anderson", "Thomas", "Jackson", "White", "Harris", "Martin"};
+        String[] names = {"John", "Emily", "Michael", "Sophia", "David", "Olivia", "Daniel", "Emma", "James", "Ava"};
+
+        Map<String, String> personMap = personMap(surnames, names);
+        System.out.println(personMap);
+    }
+}
